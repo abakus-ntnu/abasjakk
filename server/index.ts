839464303e68a515d9b5f56353c064400849e060
Arash Farzaneh Taleghani <arashfa@stud.ntnu.no>
2023-04-11 20:38:50 +0200

    Inital setup

---
+++ server/index.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv";
+import router from "./routes";
+
+dotenv.config();
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect("mongodb://127.0.0.1:27017/abasjakk", {
+    dbName: "abasjakk",
+    retryWrites: true,
+    w: "majority",
+  })
+  .then(() => console.log("connected"))
+  .catch((e) => console.log(e));
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(router);
+
+app.listen(process.env.PORT, () => {
+  console.log(`now listening on port ${process.env.PORT}`);
+});
---
+++ server/types.ts
@@ -0,0 +1,5 @@
+import type { Model, Types, Document, HydratedDocument } from "mongoose";
+
+export interface UserType extends Document {
+  _id: string;
+}
