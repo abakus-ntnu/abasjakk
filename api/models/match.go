f224c34017295e5fc78b4033137b488057988178
Falk Elvedal Bruskeland <66320400+haukkagu@users.noreply.github.com>
2023-11-02 21:50:34 +0100

    Change name from round to match (ops) (#62)

--- api/models/match.go
+++ api/models/match.go
@@ -2,18 +2,18 @@ package models
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-type RoundResult int
+type MatchResult int
 
 const (
-	InProgress RoundResult = iota
+	InProgress MatchResult = iota
 	WhiteWinner
 	BlackWinner
 	Remis
 )
 
-type Round struct {
+type Match struct {
 	Id        primitive.ObjectID `json:"id" bson:"_id"`
 	WhiteUser primitive.ObjectID `json:"white" bson:"white"`
 	BlackUser primitive.ObjectID `json:"black" bson:"black"`
-	Result    RoundResult        `json:"result" bson:"result"`
+	Result    MatchResult        `json:"result" bson:"result"`
 }
