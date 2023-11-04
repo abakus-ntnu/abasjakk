b6012261ca1a74c347678d8f1a53c65bf7dc6924
haukkagu <falk.bruskeland@gmail.com>
2023-11-04 17:05:10 +0100

    Add round endpoints

--- api/main.go
+++ api/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/abakus-ntnu/abasjakk/api/db"
 	"github.com/abakus-ntnu/abasjakk/api/handlers"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,13 +14,24 @@ func main() {
 	defer db.CloseMongoDB()
 
 	router := gin.Default()
-	router.GET("/user", handlers.HandleGetUsers)
-	router.POST("/user", handlers.HandleCreateUser)
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowHeaders = []string{"*"}
+	router.Use(cors.New(config))
+	router.GET("/user/", handlers.HandleGetUsers)
+	router.POST("/user/", handlers.HandleCreateUser)
 	router.GET("/user/:id", handlers.HandleGetUser)
 	router.PUT("/user/:id", handlers.HandleUpdateUser)
 	router.DELETE("/user/:id", handlers.HandleDeleteUser)
-	router.GET("/match", handlers.HandleGetMatches)
+	router.GET("/match/", handlers.HandleGetMatches)
 	router.GET("/match/:id", handlers.HandleGetMatch)
 	router.PUT("/match/:id", handlers.HandleUpdateMatch)
+	router.GET("/round/", handlers.HandleGetRounds)
+	router.POST("/round/", handlers.HandleCreateRound)
+	router.GET("/round/:id", handlers.HandleGetRound)
+	router.DELETE("/round/:id", handlers.HandleDeleteRound)
+	router.GET("/login", func (c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "correct password"})
+	})
 	router.Run()
 }
