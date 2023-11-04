630f4484020fbf3bcda2018df4ffda63db5efed7
Falk Elvedal Bruskeland <66320400+haukkagu@users.noreply.github.com>
2023-11-04 23:24:34 +0100

    Update user scores on match update (#66)

--- api/utils/match.go
+++ api/utils/match.go
@@ -39,3 +39,28 @@ func PopulateMatch(match models.Match) models.PopulatedMatch {
 		Table:     match.Table,
 	}
 }
+
+func ResultToScore(whitePiece bool, result models.MatchResult) float64 {
+    if (result == models.Draw) {
+        return 0.5
+    }
+    if ((whitePiece && result == models.WhiteWinner) || (!whitePiece && result == models.BlackWinner)) {
+        return 1
+    }
+    return 0
+}
+
+func UpdateScores(match models.Match, newResult models.MatchResult) {
+    populatedMatch := PopulateMatch(match)
+
+ 	white := populatedMatch.WhiteUser
+	black := populatedMatch.BlackUser
+
+	whiteDiffScore := ResultToScore(true, newResult) - ResultToScore(true, match.Result)
+	blackDiffScore := ResultToScore(false, newResult) - ResultToScore(false, match.Result)
+
+	white.Score += whiteDiffScore
+	black.Score += blackDiffScore
+	db.UpdateById("user", white.Id, white)
+	db.UpdateById("user", black.Id, black)
+}
